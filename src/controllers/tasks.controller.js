3ef4c52d03fa225b887a5e239804c6bc189af718
MiltonSuarezPrebel <136024843+MiltonSuarezPrebel@users.noreply.github.com>
2024-11-04 10:00:15 -0500

    Created frontend

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,9 +1,25 @@
 import Task from '../models/task.model.js'
+const axios = require ('axios')
 
 
 export const getTasks = async (req, res) => {}
 
-export const createTasks = async (req, res) => {}
+export const createTasks = async (req, res) => {
+    let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: 'https://offcorss.myvtex.com/api/catalog_system/pub/products/search/',
+        headers: { }
+    };
+      
+    axios.request(config)
+      .then((response) => {
+    console.log(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+}
 
 export const getTask = async (req, res) => {}
 
