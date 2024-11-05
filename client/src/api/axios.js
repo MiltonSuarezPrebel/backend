6660e948461681a17a07a154de1fa41793081b0a
MiltonSuarezPrebel <136024843+MiltonSuarezPrebel@users.noreply.github.com>
2024-11-05 01:51:58 -0500

    Update URL Mongodb

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:4000/api',
+    baseURL: 'https://backend-mongodb-w0sd.onrender.com/api',
     withCrdentials: true
 })
 
