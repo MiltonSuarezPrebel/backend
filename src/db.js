82b9c0d8194c6e070af489676bf35fe892ba6d81
MiltonSuarezPrebel <136024843+MiltonSuarezPrebel@users.noreply.github.com>
2024-11-05 01:41:00 -0500

    Prod variable

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/dev_offcorss");
+        await mongoose.connect("mongodb+srv://miltonsuarez25:tKBpadmJ1j4ospxf@offcorss.ajzu8.mongodb.net/?retryWrites=true&w=majority&appName=offcorss");
         console.log("Conectado correctamente");
     } catch (error) {
         console.log(error)
