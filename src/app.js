a259e0c6305c04d67936c7141ff5ccb74e46c0a2
MiltonSuarezPrebel <136024843+MiltonSuarezPrebel@users.noreply.github.com>
2024-11-05 08:13:45 -0500

    URL Cors

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import taskRoutes from './routes/task.routes.js'
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://vgk.lhc.mybluehost.me',
     credentials: true
 }))
 app.use(morgan('dev'));
